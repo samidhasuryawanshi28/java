e698b7de0ddbee85d1ff1e2e92f962f4d13f2dd4
samidhasuryawanshi28 <samidhasuryawanshi28@gmail.com>
2026-03-31 07:37:57 +0530

    Add files via upload

---
+++ ThreadPrioritySystem.java
@@ -0,0 +1,17 @@
+public class ThreadPrioritySystem extends Thread {
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is running");
+    }
+
+    public static void main(String[] args) {
+        ThreadPrioritySystem t1 = new ThreadPrioritySystem();
+        ThreadPrioritySystem t2 = new ThreadPrioritySystem();
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+}
