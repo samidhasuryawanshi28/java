6a6d86aca90478224ba930235c61f98d8f8d1e4b
samidhasuryawanshi28 <samidhasuryawanshi28@gmail.com>
2026-03-31 07:35:12 +0530

    Add files via upload

---
+++ DownloadManager.java
@@ -0,0 +1,14 @@
+public class DownloadManager implements Runnable {
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Downloading...");
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new DownloadManager());
+        Thread t2 = new Thread(new DownloadManager());
+
+        t1.start();
+        t2.start();
+    }
+}
